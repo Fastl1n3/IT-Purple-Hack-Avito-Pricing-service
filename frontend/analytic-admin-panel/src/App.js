572b4f499ba9d00563e5ee27fd9c96a3da48cd69
skpntsv <n.skopintsev@g.nsu.ru>
2024-03-11 14:54:21 +0700

    init react-app

---
+++ frontend/analytic-admin-panel/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button';
+
+function App() {
+	return (
+		<Button variant='primary'>
+			Web Dev blogfdssafdassddssd
+		</Button>
+	);
+}
+
+export default App;
