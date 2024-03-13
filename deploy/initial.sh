4563e973b98cd81d05214805bf4123831ee70ce7
ykhdr <yanhudorozhkov@gmail.com>
2024-03-14 02:22:25 +0700

    add creating neo4j schemas to initiash script

--- deploy/initial.sh
+++ deploy/initial.sh
@@ -3,6 +3,9 @@
 docker-compose up -d
 
 sleep 5
+
 ./postgresql-config/create-schemas.sh
 ./postgresql-config/configure-dbs.sh
 ./postgresql-config/configure-replication.sh
+
+./neo4j-config/create-schemas.sh
